757d7b3b5ba261fd2a56eda8bf8cad8551f17d9d
Elizabeth Bigger <25041285+eebigger@users.noreply.github.com>
2021-02-19 12:36:07 +0100

    updated to force light theme for now

---
+++ js/anatole-header.min.b947896d15709af409ebaf0868da033bbbd655fcc57624c0cca7bfc6a3daadb2.js
@@ -0,0 +1,5 @@
+function getTheme(){return localStorage.getItem('theme')?localStorage.getItem('theme'):null;}
+function setTheme(style){document.documentElement.setAttribute('data-theme',style);localStorage.setItem('theme',style);}
+function init(){var theme=getTheme();setTheme('light');document.documentElement.setAttribute('data-theme','light');}
+function switchTheme(e){var theme=getTheme();if(theme=='light'){setTheme('dark');}else{setTheme('light');}}
+document.addEventListener('DOMContentLoaded',function(){var themeSwitcher=document.querySelector('.theme-switch');themeSwitcher.addEventListener('click',switchTheme,false);},false);document.addEventListener("DOMContentLoaded",function(){var $navbarBurgers=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);if($navbarBurgers.length>0){$navbarBurgers.forEach(function($el){$el.addEventListener("click",function(){var target=$el.dataset.target;var $target=document.getElementById(target);$el.classList.toggle("is-active");$target.classList.toggle("is-active");});});}});init();
